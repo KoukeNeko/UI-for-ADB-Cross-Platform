4c9a20526163c3552a64bb96ab3da8ee960536b1
Violet's Foxes <111033412+VioletsFoxes@users.noreply.github.com>
2022-08-23 22:42:01 +0800

    fix device list bg color issues

--- src/components/DeviceList/index.jsx
+++ src/components/DeviceList/index.jsx
@@ -3,47 +3,40 @@ import { Command } from "@tauri-apps/api/shell";
 
 export default function DeviceList() {
     const [devices, setDevices] = React.useState([]);
-    const [devicesEle, setDevicesEle] = React.useState();
-    const [isViewNowIndex, setIsViewNowIndex] = React.useState(1);
+    const [isViewNowIndex, setIsViewNowIndex] = React.useState("");
+    const [currentInfo, setCurrentInfo] = React.useState([]);
 
     // React.useEffect(() => {
     //     alert(isViewNowIndex)
     // },[isViewNowIndex])
 
+    const TempList = ["R5CR30CVTZX device","emulator-5554 device", "emulator-5556 device"]
+
     React.useEffect(() => {
         const getCMD = async () => {
             let output = await new Command("devicesList", ["devices"]).execute();
             const devices = await String(output.stdout).split("\n");
-            setDevices(devices);
-            return devices;
+            // setDevices(devices);
+            return devices
+            
         };
 
-        getCMD().then((devices) =>
-            //make list to jsx
-            setDevicesEle(devices.map((device, index) => {
-                if(device !== "List of devices attached" && device !== '')
-                    return (
-                        <div style={{
-                            backgroundColor: (isViewNowIndex === index) ? "#87bcde" : "#243b4a", 
-                            height: "60px",
-                            width: "auto",
-                            overflow: "hidden",
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            cursor: "pointer",
-                        }}
-                        onClick={() => {
-                            setIsViewNowIndex(index)
-                            alert(isViewNowIndex === index, index)
-                        }}
-                        >
-                            {device}{isViewNowIndex === index}
-                        </div>
-                    )
-            }))
-        );
-    }, []);
+        getCMD()
+        .then((devices => {
+            setDevices(devices)
+            if (isViewNowIndex === "")
+            setIsViewNowIndex(devices[1])
+        }))
+        
+
+        setTimeout(() => {
+            getCMD()
+            .then((devices => setDevices(devices)))
+        }, 5000);
+    });
+
+    
+
 
     return <div style={{
         display: "flex",
@@ -53,26 +46,74 @@ export default function DeviceList() {
         width: "750px",
         left: 0,
         top: 45,
+        zIndex: 0
     }}>
         <div style={{
             width: "280px",
             display: "flex",
             flexDirection: "column",
-            marginLeft: "50px",
+            marginLeft: "30px",
+            gap: "10px",
             
         }}>
-            {/* divice list */}
+
             <h2>Devices List</h2>
-            {devicesEle}
+
+            {
+                devices.map((device) => {
+                    const index = device
+                    // if (isViewNowIndex === "null")
+                    //     setIsViewNowIndex(index)
+
+                    if(device !== "List of devices attached" && device !== '')
+                        return (
+                            <div 
+                                key={index}
+                                style={{
+                                backgroundColor: (isViewNowIndex === index) ? "#87bcde" : "#243b4a", 
+                                height: "45px",
+                                marginRight: "15px", // to fix wired shader issue
+                                width: "auto",
+                                overflow: "hidden",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                cursor: "pointer",
+                                transition: "all 200ms linear",
+                                zIndex: (isViewNowIndex === index) ?  1000 : 1 ,
+                                borderRadius: "15px",
+                                boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, .4) " ,
+                                
+                                
+                            }}
+                            onClick={() => {
+                                setIsViewNowIndex(index)
+                                // const getCMD = async () => {
+                                //     let output = await new Command("devicesList", ["devices"]).execute();
+                                //     const devices = await String(output.stdout).split("\n");
+                                //     setDevices(devices);
+                                // };
+                                // getCMD();
+                            }}
+                            >
+                                {device}
+                            </div>
+                        )
+                })
+            }
         </div>
         <div style={{
             backgroundColor: "#87bcde",
             width: "60%",
-            marginRight: "10%",
+            marginRight: "6%",
             marginBottom: "10%",
+            zIndex: 100,
+            boxShadow: "0px 0px 5px 1px rgba(0, 0, 0, .4) " ,
+            borderRadius: "15px", 
+            // marginLeft: "-1px",
         }}>
             {/* detials */}
-            <h2>Devices List</h2>
+            <h2>Devices Info</h2>
             {isViewNowIndex}
         </div>
     </div>;
